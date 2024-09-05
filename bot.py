df658a3b67817c7a3de9dfdb84ae0f094e7503a6
Jess <jlswans1@gmail.com>
2024-09-04 22:42:42 -0700

    add helldivers preliminary functionality.  this bot is now above its station.

--- bot.py
+++ bot.py
@@ -335,4 +335,208 @@ async def gimme_salt(ctx):
 #     if 'you like?' in message.content.lower():
 #         await message.channel.send('You like this?')
 
+# ====================================
+#
+# HELLDIVERS BOT BELOW THIS LINE
+#
+# ====================================
+
+armor_weight = [
+    'Light',
+    'Medium',
+    'Heavy'
+]
+
+armor_passive = [
+    'Democracy Protects',
+    'Electrical Conduit',
+    'Engineering Kit',
+    'Extra Padding',
+    'Fortified',
+    'Inflammable',
+    'Med-Kit',
+    'Peak Physique',
+    'Scout',
+    'Servo-Assisted'
+]
+
+primary_weapon = [
+    'AR-23 Liberator',
+    'AR-23P Liberator',
+    'Penetrator',
+    'AR-23C Liberator Concussive',
+    'AR-23A Liberator Carbine',
+    'AR-61 Tenderizer',
+    'BR-14 Adjudicator',
+    'R-63 Diligence',
+    'R-63CS Diligence Counter Sniper',
+    'SMG-37 Defender',
+    'SMG-72 Pummeler',
+    'SG-8 Punisher',
+    'SG-8S Slugger',
+    'SG-451 Cookout',
+    'SG-225 Breaker',
+    'SG-225SP Breaker Spray & Pray',
+    'SG-225IE Breaker Incendiary',
+    'CB-9 Exploding Crossbow',
+    'JAR-5 Dominator',
+    'R-36 Eruptor',
+    'SG-8P Punisher Plasma',
+    'ARC-12 Blitzer',
+    'LAS-5 Scythe',
+    'LAS-16 Sickle',
+    'PLAS-1 Scorcher',
+    'PLAS-101 Purifier',
+    'FLAM-66 Torcher'
+]
+
+secondary_weapon = [
+    'P-2 Peacemaker',
+    'P-19 Redeemer',
+    'P-113 Verdict',
+    'P-4 Senator',
+    'SG-22 Bushwhacker',
+    'P-72 Crisper',
+    'GP-31 Grenade Pistol',
+    'LAS-7 Dagger'
+]
+
+throwable = [
+    'G-6 Frag',
+    'G-12 High Explosive',
+    'G-10 Incendiary',
+    'G-16 Impact',
+    'G-13 Incendiary Impact',
+    'G-23 Stun',
+    'G-3 Smoke',
+    'G-123 Thermite',
+    'K-2 Throwing Knife'
+]
+
+stratagem = [
+    'Eagle 500kg Bomb',
+    'Orbital Gatling Barrage',
+    'Eagle Airstrike',
+    'Orbital Walking Barrage',
+    'Eagle Strafing Run',
+    'Orbital EMS Strike',
+    'Eagle Smoke Strike',
+    'Orbital Gas Strike',
+    'Orbital Railcannon Strike',
+    'Eagle Cluster Bomb',
+    'Orbital Smoke Strike',
+    'Orbital Airburst Strike',
+    'Orbital Laser',
+    'Orbital Precision Strike',
+    'Orbital 380mm HE Barrage',
+    'Eagle Napalm Strike',
+    'Orbital 120mm HE Barrage',
+    'Eagle 110mm Rocket Pods',
+    'MLS-4X Commando',
+    'EXO-49 Emancipator Exosuit',
+    'M-105 Stalwart',
+    'AC-8 Autocannon',
+    'EXO-45 Patriot Exosuit',
+    'MG-206 Heavy Machine Gun',
+    'SH-32 Shield Generator Pack',
+    'RL-77 Airburst Rocket Launcher',
+    'RS-422 Railgun',
+    'LAS-98 Laser Cannon',
+    'ARC-3 Arc Thrower',
+    'AX/AR-23 "Guard Dog"',
+    'FAF-14 Spear',
+    'GL-21 Grenade Launcher',
+    'MG-43 Machine Gun',
+    'SH-20 Ballistic Shield Backpack',
+    'B-1 Supply Pack',
+    'FLAM-40 Flamethrower',
+    'AX/LAS-5 "Guard Dog" Rover',
+    'GR-8 Recoilless Rifle',
+    'LIFT-850 Jump Pack',
+    'APW-1 Anti-Materiel Rifle',
+    'EAT-17 Expendable Anti-Tank',
+    'LAS-99 Quasar Cannon',
+    'MD-17 Anti-Tank Mines',
+    'FX-12 Shield Generator Relay',
+    'A/M-23 EMS Mortar Sentry',
+    'A/MLS-4X Rocket Sentry',
+    'A/G-16 Gatling Sentry',
+    'A/ARC-3 Tesla Tower',
+    'A/M-12 Mortar Sentry',
+    'MD-6 Anti-Personnel Minefield',
+    'A/MG-43 Machine Gun Sentry',
+    'A/AC-8 Autocannon Sentry',
+    'MD-14 Incendiary Mines',
+    'E/MG-101 HMG Emplacement'
+]
+
+booster = [
+    'Vitality Enhancement',
+    'Stamina Enhancement',
+    'Muscle Enhancement',
+    'UAV Recon Booster',
+    'Increased Reinforcement Budget',
+    'Flexible Reinforcement Budget',
+    'Hellpod Space Optimization',
+    'Localization Confusion',
+    'Expert Extraction Pilot',
+    'Motivational Shocks',
+    'Experimental Infusion',
+    'Firebomb Hellpods'
+]
+
+mission_type = [
+    'Blitz: Search and Destroy',
+    'Conduct Geological Survey',
+    'Emergency Evacuation',
+    'Evacuate High-Value Assets (Defense Exclusive)',
+    'Launch ICBM',
+    'Retrieve Essential Personnel (Defense Exclusive)',
+    'Retrieve Valuable Data',
+    'Spread Democracy',
+    'Purge Hatcheries','Enable E-710 Extraction',
+    'Nuke Nursery',
+    'Eradicate Terminid Swarm',
+    'Sabotage Air Base',
+    'Eradicate Automaton Forces',
+    'Destroy Command Bunkers',
+    'Neutralize Orbital Defenses'
+]
+
+@bot.command(name="freedom", help="Test your commitment to democracy, Helldiver.")
+async def random_loadout(ctx):
+    def get_formatted_stratagems(list):
+        stratagem_list = random.sample(list, 4)
+        stratagem_string = ''
+
+        for item in stratagem_list:
+            stratagem_string += item + '\n'
+        return stratagem_string
+
+    message = f"""
+=======================
+**FREEDOM LOADOUT**
+=======================
+**Primary weapon:** {random.choice(primary_weapon)}
+**Secondary weapon:** {random.choice(secondary_weapon)}
+**Throwable:** {random.choice(throwable)}
+
+**Armor weight:** {random.choice(armor_weight)}
+**Armor passive:** {random.choice(armor_passive)}
+
+**Stratagems:**
+{get_formatted_stratagems(stratagem)}
+**Booster:** {random.choice(booster)}
+    """
+
+    await ctx.send(message)
+
+@bot.command(name="mission", help="Where are you needed most in Super Earth's defense of the galaxy?")
+async def random_mission(ctx):
+    message = f"""
+    Your mission is this: **{random.choice(mission_type)}** anywhere in the galaxy where managed democracy is under threat. Our continued freedom is counting on your success, Helldiver. Don't let us down!
+    """
+
+    await ctx.send(message)
+
 bot.run(TOKEN)

77e210a20a9f476fd571e54a8cbcd50bb6af3926
Jess <jlswans1@gmail.com>
2024-09-04 20:58:35 -0700

    create roulette game for dbd

--- bot.py
+++ bot.py
@@ -335,4 +335,132 @@ async def gimme_salt(ctx):
 #     if 'you like?' in message.content.lower():
 #         await message.channel.send('You like this?')
 
+# ------------------------------------
+# MAP ROULETTE CODE BELOW THIS COMMENT
+# ------------------------------------
+
+roundStarted = False
+roulette = []
+realms = [
+    'The MacMillan Estates',
+    'Autohaven Wreckers',
+    'Coldwind Farm',
+    'Crotus Prenn Asylum',
+    'Haddonfield',
+    'Backwater Swamp',
+    'Lery\'s Memorial Institute',
+    'Red Forest',
+    'Springwood',
+    'Gideon Meat Plant',
+    'Yamaoka Estate',
+    'Ormond',
+    'Grave of Glenvale',
+    'Silent Hill',
+    'Raccoon City',
+    'Forsaken Boneyard',
+    'Withered Isle',
+]
+
+# begin a round of map roulette
+@bot.command(name="round", help="Start a new round of map roulette!  You will automatically be added to the round when using this command.")
+async def start_round(ctx):
+    author = ctx.author
+
+    if len(roulette):
+        await ctx.send('There\'s already a round in progress!  `?endround` will end the current round of map roulette.')
+    else:
+        # add the creator to the round
+        # id is member ID
+        roulette.append({"player": author})
+
+        await ctx.send(f"""
+**A new round of roulette has begun!**
+Blame {author.mention} for your misfortune.
+""")
+
+@bot.command(name="join", help="Join the round of roulette.  The first four people to use this command will be entered into the round!")
+async def add_to_round(ctx):
+    author = ctx.author
+
+    list_of_joined = [True for elem in roulette if author in elem.values()]
+
+    if roundStarted == True:
+        await ctx.send(f'The round has already started!  Sit tight for the next one.')
+    elif any(list_of_joined):
+        await ctx.send(f'You\'re already in this round, {author.mention}!')
+    elif len(roulette) == 4:
+        await ctx.send(f'Sorry, {author.mention}, this round is full!')
+    else:
+        # add the joiner to the round
+        roulette.append({"player": author})
+
+        await ctx.send(f'{author.mention} has joined the round!')
+
+@bot.command(name="start", help="Start the game!")
+async def start_roulette(ctx):
+    global roundStarted
+    message = ''
+    listMessage = f"""
+**Roulette is about to begin!**
+Choose from the list of realms below to participate:\n
+1. The MacMillan Estates
+2. Autohaven Wreckers
+3. Coldwind Farm
+4. Crotus Prenn Asylum
+5. Haddonfield
+6. Backwater Swamp
+7. Lery's Memorial Institute
+8. Red Forest
+9. Springwood
+10. Gideon Meat Plant
+11. Yamaoka Estate
+12. Ormond
+13. Grave of Glenvale
+14. Silent Hill
+15. Raccoon City
+16. Forsaken Boneyard
+17. Withered Isle\n
+Reply with the number of the realm you'd like to pick, and let the games begin!"""
+
+    if roundStarted == True:
+        #if the round has been started
+        message += f'The round has already started!  Sit tight for the next one.'
+    elif roundStarted == False and len(roulette) > 0:
+        # if the round has not been started AND there is at least one player in the list
+        message += f"""**The game is afoot!**\n
+These are your players:\n"""
+
+        for item in roulette:
+            message += f"""{item['player'].mention}\n"""
+            await item['player'].send(listMessage)
+
+        roundStarted = True
+    elif roundStarted == False or len(roulette) == 0:
+        # if the round has not been started OR there are no players in the list
+        message += f"""There's no round in progress yet!  `?round` starts a new round of roulette."""
+    else:
+        message += 'Whoops!  Something went wrong.  Let Jess know by tagging her in a reply to this message.'
+
+    await ctx.send(message)
+
+    def check(msg):
+        return msg.author == ctx.author
+
+    # TODO: This still hits the timeout when replying to dm, need to fix.
+    msg = await client.wait_for('message', check=check, timeout=30.0)
+    reply = int(msg.lower) - 1
+
+    rouletteItem = next(item for items in roulette if item['id'] == msg.author['id'])
+    rouletteItem = rouletteItem["realm": realms[reply]]
+    print(rouletteItem)
+
+@bot.command(name="clear", help="Clear the current round.  This deletes all players and choices from the round without resolving the round.")
+async def clear_roulette(ctx):
+    roulette.clear()
+
+    global roundStarted
+    roundStarted = False
+
+    await ctx.send(f'Cleared the round.')
+
 bot.run(TOKEN)
